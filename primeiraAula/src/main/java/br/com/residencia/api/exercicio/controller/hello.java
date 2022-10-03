a69f5f9ff5836d5801d19b87c38759a3556c3d2f
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-03 19:26:38 -0300

    Testandos os request mapping e Get mapping

--- primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/hello.java
+++ primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/hello.java
@@ -1,12 +1,20 @@
 package br.com.residencia.api.exercicio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class hello {
-    @RequestMapping("/")
+    @GetMapping("/hello")
     public String teste() {
         return "Hello World";
+    }
+
+
+    @GetMapping("/reposta")
+    public String reposta() {
+        return "Teste";
     }
 }
