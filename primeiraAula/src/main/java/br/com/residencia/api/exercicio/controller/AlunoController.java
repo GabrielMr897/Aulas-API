ea98d799fa8e9023fc4d30bab47aec8f385cffe6
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-03 21:57:43 -0300

    Método put e delete

--- primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/AlunoController.java
+++ primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/AlunoController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -49,4 +51,26 @@ public class AlunoController {
         alunos.add(aluno);
         return aluno;
     }
+
+    @DeleteMapping("{matricula}")
+	public void apagar(@PathVariable String matricula) {
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).getMatricula().equals(matricula)) {
+				alunos.remove(i);
+			}
+		}
+
+	}
+
+
+    @PutMapping("{matricula}")
+	public Aluno atualizar(@RequestBody Aluno aluno, @PathVariable String matricula) {
+		for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).getMatricula().equals(matricula)) {
+				alunos.set(i, aluno);
+				return aluno;
+			}
+		}
+		return null;
+	}
 }
