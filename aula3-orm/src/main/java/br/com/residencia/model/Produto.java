4d754460978b3106423525eca3e524b11eaed5c6
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-04 19:36:19 -0300

    Mexendo na com banco de dados na api

---
+++ aula3-orm/src/main/java/br/com/residencia/model/Produto.java
@@ -0,0 +1,80 @@
+package br.com.residencia.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "produto", schema = "public")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private long id;
+
+    @Column(nullable = false, length = 40)
+    private String descricao;
+
+    @Column
+    private BigDecimal valor;
+
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    
+    @Override
+    public String toString() {
+        return "Produto [id=" + id + ", descricao=" + descricao + ", valor=" + valor + ", dataCadastro=" + dataCadastro
+                + "]";
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
