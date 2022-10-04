0fe211493c62042921c81f6c232039a51b2cceb5
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-03 21:27:14 -0300

    Método post

--- primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/AlunoController.java
+++ primeiraAula/src/main/java/br/com/residencia/api/exercicio/controller/AlunoController.java
@@ -5,9 +5,13 @@ import br.com.residencia.api.exercicio.model.Aluno;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,5 +40,13 @@ public class AlunoController {
 			}
 		}
 		return null;
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Aluno inserir(@RequestBody Aluno aluno) {
+        alunos.add(aluno);
+        return aluno;
     }
 }
