438b498e11b30702466c124a17f8bf1f166301a3
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-05 20:26:11 -0300

    autowired no pagamento

--- aula4/src/main/java/br/com/residencia/model/Pagamento.java
+++ aula4/src/main/java/br/com/residencia/model/Pagamento.java
@@ -1,17 +1,18 @@
 package br.com.residencia.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Pagamento {
+
+    @Autowired
     private Consulta consulta;
     
+    @Autowired
     private Exame exame;
     
-    public Pagamento(Consulta consulta, Exame exame) {
-        this.consulta = consulta;
-        this.exame = exame;
-    }
+    
     public Double calcularProcedimentos(Double valorConsulta, Double valorExame) {
         return consulta.calcularValorConsulta(valorConsulta) + exame.calcularValorExame(valorExame);
     }
