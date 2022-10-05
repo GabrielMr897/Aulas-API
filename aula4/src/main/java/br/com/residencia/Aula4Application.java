da4e3ba1f12cb8352de7edc72b01f83a8d18330c
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-05 19:43:22 -0300

    Usando autowired

--- aula4/src/main/java/br/com/residencia/Aula4Application.java
+++ aula4/src/main/java/br/com/residencia/Aula4Application.java
@@ -1,15 +1,18 @@
 package br.com.residencia;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.residencia.model.Consulta;
-import br.com.residencia.model.Exame;
+
 import br.com.residencia.model.Pagamento;
 
 @SpringBootApplication
 public class Aula4Application implements CommandLineRunner {
+
+	@Autowired
+	private Pagamento pagamento;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aula4Application.class, args);
@@ -17,9 +20,6 @@ public class Aula4Application implements CommandLineRunner {
 
 	@Override
 	public void	run(String... args) throws Exception {
-		Consulta consulta = new Consulta();
-		Exame exame = new Exame();
-		Pagamento pagamento = new Pagamento(consulta, exame);
 
 		System.out.println("Total a pagar" + " " + pagamento.calcularProcedimentos(200., 80.));
 	}
