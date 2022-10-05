2f03e74480c19bd6223ad8ff6a73c91458ecc490
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-04 22:21:14 -0300

    armazenando direto no banco de dados

--- aula3-orm/src/main/java/br/com/residencia/controller/ProdutoController.java
+++ aula3-orm/src/main/java/br/com/residencia/controller/ProdutoController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.residencia.model.Produto;
@@ -34,4 +39,21 @@ public class ProdutoController {
 
       return ResponseEntity.notFound().build();
     }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto inserir(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+
+    @DeleteMapping("{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!produtoRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		produtoRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 }
