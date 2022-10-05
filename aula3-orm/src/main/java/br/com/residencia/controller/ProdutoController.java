4206fe08e45b89209a7d9a51773a06f6323d4cd0
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-04 21:10:01 -0300

    buscando produto do banco de dados

--- aula3-orm/src/main/java/br/com/residencia/controller/ProdutoController.java
+++ aula3-orm/src/main/java/br/com/residencia/controller/ProdutoController.java
@@ -1,12 +1,37 @@
 package br.com.residencia.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.residencia.model.Produto;
 import br.com.residencia.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+
+    @Autowired
     private ProdutoRepository produtoRepository;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> buscar(@PathVariable Long id) {
+      Optional<Produto> produto = produtoRepository.findById(id);
+      if(produto.isPresent()) {
+        return ResponseEntity.ok(produto.get());
+      }
+
+      return ResponseEntity.notFound().build();
+    }
 }
