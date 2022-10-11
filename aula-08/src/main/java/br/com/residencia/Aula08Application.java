264b372219dca2c0225962c282728c6d2b9c74ed
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-11 19:00:39 -0300

    criando mais tabelas com mappedsuperclass

--- aula-08/src/main/java/br/com/residencia/Aula08Application.java
+++ aula-08/src/main/java/br/com/residencia/Aula08Application.java
@@ -1,4 +1,4 @@
-package br.com.residencia.aula08;
+package br.com.residencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- aula-08/src/test/java/br/com/residencia/Aula08ApplicationTests.java
+++ aula-08/src/test/java/br/com/residencia/Aula08ApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.residencia.aula08;
+package br.com.residencia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
