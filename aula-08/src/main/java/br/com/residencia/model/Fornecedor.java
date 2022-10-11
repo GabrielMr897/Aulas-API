4e28f85440519fdbb70ef95949c35dfa089792f3
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-11 19:38:59 -0300

    criando applications

--- aula-08/src/main/java/br/com/residencia/model/Fornecedor.java
+++ aula-08/src/main/java/br/com/residencia/model/Fornecedor.java
@@ -1,11 +1,16 @@
 package br.com.residencia.model;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
-@MappedSuperclass
+@Inheritance(strategy =  InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_fornecedor")
+@Entity
 public class Fornecedor {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
